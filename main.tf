caf2fb45ee9cdf5e2aa8db97a0d02240d69c813a
s11kyin <richiekyin@gmail.com>
2026-03-12 07:24:02 +0000

    modified main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 module "vpc" {
-  source              = "git@github.com:s11kyin/kay-module-resource.git//modules/vpc?ref=main"
+  source              = "git@github.com:s11kyin/kay-module-resource.git//vpc?ref=main"
   vpc_cidr            = var.vpc_cidr
   public_subnet_cidr  = var.public_subnet_cidr
   private_subnet_cidr = var.private_subnet_cidr
 }
 
 module "ec2_public" {
-  source        = "git@github.com:s11kyin/kay-module-resource.git//modules/ec2?ref=main"
+  source        = "git@github.com:s11kyin/kay-module-resource.git//ec2?ref=main"
   vpc_id        = module.vpc.vpc_id
   subnet_id     = module.vpc.public_subnet_id
   instance_type = var.instance_type
@@ -16,7 +16,7 @@ module "ec2_public" {
 }
 
 module "ec2_private" {
-  source        = "git@github.com:s11kyin/kay-module-resource.git//modules/ec2?ref=main"
+  source        = "git@github.com:s11kyin/kay-module-resource.git//ec2?ref=main"
   vpc_id        = module.vpc.vpc_id
   subnet_id     = module.vpc.private_subnet_id
   instance_type = var.instance_type
